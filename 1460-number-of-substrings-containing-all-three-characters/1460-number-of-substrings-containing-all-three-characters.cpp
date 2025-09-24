f2c4b6376453f5eac3f1716055fb5c78b36aa9c5
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-09-24 13:06:22 -0700

    Time: 3 ms (89.66%), Space: 11.1 MB (56.94%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        //optimal approach
+        //find the minimum index where we found abc and then count all the sub-strings uptil the index 0 i.e. starting
+        int n = s.length();
         int l=0, r=0, cnt=0;
-        int hash[3]={0};
-        while(r<s.size()) {
-            hash[s[r]-'a']++;
-            while(hash[0]>=1 && hash[1]>=1 && hash[2]>=1) {
-                cnt += (s.size()-r);
-                hash[s[l]-'a']--;
-                l++;
+        int hash[3] = {-1,-1,-1};
+        while(r<n) {
+            hash[s[r]-'a'] = r;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1) {
+                cnt = cnt + (1+min(min(hash[0], hash[1]), hash[2]));
             }
-            // if(hash[0]>=1 && hash[1]>=1 && hash[2]>=1){
-            //     cnt += (s.size()-r);
-            // }
             r++;
         }
         return cnt;
