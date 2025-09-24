f2342d3280474071bcd113b11cc4b25c82dcdc47
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-09-24 14:21:58 -0700

    Time: 0 ms (100%), Space: 9.8 MB (26.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if(s.length() != t.length()) return false;
+        int hash[26] = {0};
+        int i = 0;
+        while(i<s.length()){
+            hash[s[i]-'a']++;
+            hash[t[i]-'a']--;
+            i++;
+        }
+        for(int i=0; i<26; i++) {
+            if(hash[i]!=0) return false;
+        }
+        return true;
     }
 };
