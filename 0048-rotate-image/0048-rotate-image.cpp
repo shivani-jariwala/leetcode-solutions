b7afb129350e150ecce86506e2d6785e60dea21a
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-09-30 21:40:59 -0700

    Time: 0 ms (100%), Space: 10.2 MB (68.75%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private: 
+    void reverseFun(vector<int>& arr){
+        int p1=0, p2=arr.size()-1;
+        while(p1<p2){
+            swap(arr[p1],arr[p2]);
+            p1++;
+            p2--;
+        }
+    }
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //transpose the matrix and then reverse
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=i+1; j<=matrix.size()-1; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        //now reverse it
+        for(int i=0;i<matrix.size();i++){
+            reverseFun(matrix[i]);
+        }
+    }
+};
