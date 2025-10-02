ea77bcb2a9390d47d2e848308c7df95f62e8aa7c
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-10-01 22:18:36 -0700

    Time: 2 ms (60.45%), Space: 13.9 MB (90.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,20 @@
 class Solution {
 private: 
-    void helper(vector<int> arr, int index, int target, vector<int> &singleAns, vector<vector<int>> &finalAns) {
-        if(target<=0 || index==arr.size()) {
-            if(target == 0) finalAns.push_back(singleAns);
+    void helper(vector<int>& nums, vector<vector<int>> &finalAns, vector<int> &ans, int target, int idx){
+        if(target<=0 || idx==nums.size()) {
+            if(target==0) finalAns.push_back(ans);
             return;
         }
-        singleAns.push_back(arr[index]);
-        helper(arr, index, target-arr[index], singleAns, finalAns);
-        // helper(arr, index+1, target-arr[index], singleAns, finalAns);
-        singleAns.pop_back();
-        // int currEle = arr[index];
-        // while(index<arr.size() && currEle == arr[index]) {
-        //     index++;
-        // }
-        helper(arr, index+1, target, singleAns, finalAns);
-        return;
+        ans.push_back(nums[idx]);
+        helper(nums, finalAns, ans, target-nums[idx], idx);
+        ans.pop_back();
+        helper(nums, finalAns, ans, target, idx+1);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> singleAns;
         vector<vector<int>> finalAns;
-        // vector<vector<int>> ans;
-        helper(candidates, 0, target, singleAns, finalAns);
-        // for(auto vec: finalAns) {
-        //     ans.push_back(vec);
-        // }
+        vector<int> ans;
+        helper(candidates, finalAns, ans, target, 0);
         return finalAns;
     }
 };
