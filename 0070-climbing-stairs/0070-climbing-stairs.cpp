8024fc4b55a94deabbc54041ec0604a35161796d
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-10-09 22:37:51 -0700

    Time: 0 ms (100%), Space: 8.5 MB (51.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private:
-    int computeSteps(int n, vector<int>&dp){
+    int computeSteps(int n, vector<int> &dp){
         if(n==0) return 1;
         if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int oneJump = computeSteps(n-1, dp);
-        int twoJump = computeSteps(n-2, dp);
-        dp[n] = oneJump+twoJump;
-        return oneJump + twoJump;
+        if(dp[n]) return dp[n];
+        int left = computeSteps(n-1, dp);
+        int right = computeSteps(n-2, dp);
+        int sum = left+right;
+        dp[n] = sum;
+        return sum;
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+1);
         return computeSteps(n, dp);
     }
 };
