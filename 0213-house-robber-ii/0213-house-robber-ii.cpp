5a4375e40db435caff650b9cfd87c52f5c0d0a99
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-10-19 15:03:03 -0700

    Time: 0 ms (100%), Space: 11.2 MB (8.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 private:
-    int helper(int i, vector<int>& arr, vector<int>& dp) {
-        if(i==0) return arr[0];
+    int helper(int i, vector<int>& arr, vector<vector<int>>& dp, bool isLastElePicked) {
+        if(i==0) {
+            if(isLastElePicked) return 0;
+            return arr[0];
+        }
         if(i<0) return 0; 
-        if(dp[i]!=-1) return dp[i];
-        int pick = arr[i] + helper(i-2, arr, dp);
-        int noPick = 0 + helper(i-1, arr, dp);
-        dp[i] = max(pick, noPick);
-        return max(pick, noPick);
+        if(dp[i][isLastElePicked]!=-1) return dp[i][isLastElePicked];
+        if(i == arr.size()-1) isLastElePicked = true;
+        int pick = arr[i] + helper(i-2, arr, dp, isLastElePicked);
+        if(i == arr.size()-1) isLastElePicked = false;
+        int noPick = 0 + helper(i-1, arr, dp, isLastElePicked);
+        return dp[i][isLastElePicked] = max(pick, noPick);
     }
 public:
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        vector<int> temp1;
-        vector<int> temp2;
-        for(int i=0; i<nums.size(); i++) {
-            if(i!=nums.size()-1) temp1.push_back(nums[i]);
-            if(i!=0) temp2.push_back(nums[i]);
-        }
-        vector<int> dp1(nums.size()+1, -1);
-        vector<int> dp2(nums.size()+1, -1);
-        return max(helper(temp1.size()-1, temp1, dp1), helper(temp2.size()-1, temp2, dp2));
+        vector<vector<int>> dp(nums.size()+1, vector<int>(2, -1));
+        return helper(nums.size()-1, nums, dp, false);
     }
 };
