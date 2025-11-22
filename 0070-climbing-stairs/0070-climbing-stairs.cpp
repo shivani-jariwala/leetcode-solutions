966042aa61d089ca68a6e94d45db604700161f5a
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-11-22 15:18:53 -0800

    Time: 0 ms (100%), Space: 8.7 MB (17.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,9 +6,7 @@ private:
         if(dp[n]) return dp[n];
         int left = computeSteps(n-1, dp);
         int right = computeSteps(n-2, dp);
-        int sum = left+right;
-        dp[n] = sum;
-        return sum;
+        return dp[n] = left+right;
     }
 public:
     int climbStairs(int n) {
