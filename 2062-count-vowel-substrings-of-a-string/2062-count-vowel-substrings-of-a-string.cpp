edac0a59f6363f6c404af0250103785bbc43c9a5
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-11-26 12:46:42 -0800

    Time: 0 ms (100%), Space: 8.6 MB (75.88%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,22 +1,37 @@
 class Solution {
+private: 
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
 public:
     int countVowelSubstrings(string word) {
-        int n =word.length();
-        unordered_map<char, int> mpp; // 'a':1
+        unordered_map<char, int> mpp;
+        int j =0;
         int cnt = 0;
-        for(int i =0; i<n; i++) {
-            for(int j=i; j<n;j++){
-                if(word[j]!='a' && word[j]!='e' && word[j]!='i' && word[j]!='o' &&  word[j]!='u') {
-                    //not vowel
-                    break;
-                }else {
-                    mpp[word[j]]++;
-                    if(mpp['a']>=1 && mpp['e']>=1 && mpp['i']>=1 && mpp['o']>=1 && mpp['u']>=1) {
-                        cnt ++;
-                    }
+        int n = word.length();
+        string vowel = "aeiou";
+        for(char it: vowel) {
+            mpp[it]=-1;
+        }
+
+        for(int i=0; i<n;i++) {
+            if(!isVowel(word[i])) {
+                //consonant
+                j = i+1;
+                for(char it: vowel) {
+                    mpp[it]=-1;
+                }
+            }else{
+                //vowel
+                mpp[word[i]]=i;
+                int min_last_seen = INT_MAX;
+                for(char it: vowel ) {
+                    min_last_seen = min(min_last_seen, mpp[it]);
+                }
+                if(min_last_seen>=j){
+                    cnt += (min_last_seen-j+1);
                 }
             }
-            mpp.clear();
         }
         return cnt;
     }
