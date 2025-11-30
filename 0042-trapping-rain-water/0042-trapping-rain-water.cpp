4c6721c2a2fa0ed6097746037d8d101f61e63d12
Shivani Jariwala <jariwalashivani1105@gmail.com>
2025-11-29 18:03:50 -0800

    Time: 0 ms (100%), Space: 27.2 MB (39.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,28 @@
 class Solution {
+private:
+    void computePrefixAndSuffixMax(vector<int>& height, vector<int>& pMax, vector<int>& sMax) {
+        int n = height.size();
+        for(int i=1; i<n; i++) {
+            pMax[i] = max(pMax[i-1], height[i]);
+            sMax[n-i-1] = max(sMax[n-i], height[n-i-1]);
+        }
+    }
 public:
     int trap(vector<int>& height) {
-        //TC=o(n) and SC=o(1) 2-pointer approach
-        int n=height.size();
-        int low=0, high=n-1, lMax=0, rMax=0, total=0;
-        while(low<=high) {
-            if(height[low]<=height[high]){
-                //low is small therefore move it
-                if(lMax>height[low]){
-                    total += lMax - height[low];
-                }else{
-                    lMax = height[low];
-                }
-                low++;
-            }else{
-                //high is small therefore move it
-                if(rMax>height[high]){
-                    total += rMax - height[high];
-                }else{
-                    rMax = height[high];
-                }
-                high--;
-            }
+        //    [0,1,0,2,1,0,1,3,2,1,2,1]
+        //p = [0,1,1,2,2 2 2 3 3 3 3 3]
+        //s=  [3 3 3 3 3 3 3 3 3 2 2 2 1]
+        int n = height.size();
+        int storage=0;
+        int total=0;
+        vector<int> pMax(n);
+        pMax[0] = height[0];
+        vector<int> sMax(n);
+        sMax[n-1] = height[n-1];
+        computePrefixAndSuffixMax(height, pMax, sMax);
+        for(int i=0; i<n; i++) {
+            int storage = min(pMax[i],sMax[i])-height[i];
+            if(storage>0) total+=storage; 
         }
         return total;
     }
